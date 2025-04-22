18f9376f32561fdfb74766d4aaac713b58a55a44
Advyth Ashok <advyth.ashok@landmarkgroup.com>
2025-04-22 14:56:49 +0530

    Added more icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,4 +2,11 @@ export const icons = {
   home: require('./home.png'),
   search: require('./search.png'),
   cart: require('./shopping.png'),
+  heart: require('./heart.png'),
+  'heart-outline': require('./heart.png'),
+  star: require('./star.png'),
+  back: require('./back.png'),
+  plus: require('./search.png'),
+  minus: require('./search.png'),
+  close: require('./close.png'),
 };
